1ab738c26edad42cd5a1a1a389f13055499f8773
AnnyBerrio <114838084+AnnyBerrio@users.noreply.github.com>
2022-11-25 16:42:16 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -1,19 +1,16 @@
 """
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
-
 Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
 correctamente. Tenga en cuenta datos faltantes y duplicados.
-
 """
+from datetime import datetime
 import pandas as pd
+import re
 
 
 def clean_data():
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-   df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
     
     #Eliminar datos duplicados y faltantes
     df.dropna(inplace=True)
